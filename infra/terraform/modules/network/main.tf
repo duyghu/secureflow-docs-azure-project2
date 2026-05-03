821641b4c5bc0a2656de73407c61fa53ccc51860
duyghu <esgerovaduygu2006@gmail.com>
2026-05-03 11:05:25 +0400

    Add NAT gateway for private telemetry egress

--- infra/terraform/modules/network/main.tf
+++ infra/terraform/modules/network/main.tf
@@ -27,6 +27,39 @@ resource "azurerm_subnet" "data" {
   private_endpoint_network_policies = "Disabled"
 }
 
+resource "azurerm_public_ip" "nat" {
+  name                = "pip-${var.name_prefix}-nat"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+resource "azurerm_nat_gateway" "compute" {
+  name                    = "nat-${var.name_prefix}-compute"
+  location                = var.location
+  resource_group_name     = var.resource_group_name
+  sku_name                = "Standard"
+  idle_timeout_in_minutes = 10
+  tags                    = var.tags
+}
+
+resource "azurerm_nat_gateway_public_ip_association" "compute" {
+  nat_gateway_id       = azurerm_nat_gateway.compute.id
+  public_ip_address_id = azurerm_public_ip.nat.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "web" {
+  subnet_id      = azurerm_subnet.web.id
+  nat_gateway_id = azurerm_nat_gateway.compute.id
+}
+
+resource "azurerm_subnet_nat_gateway_association" "api" {
+  subnet_id      = azurerm_subnet.api.id
+  nat_gateway_id = azurerm_nat_gateway.compute.id
+}
+
 resource "azurerm_network_security_group" "web" {
   name                = "nsg-${var.name_prefix}-web"
   location            = var.location
